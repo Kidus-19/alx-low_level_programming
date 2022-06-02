ff45ead8294b36a30d06c6e039f63fd23a305728
Kidus-19 <kidusfesseha@gmail.com>
2022-06-02 15:19:09 -0700

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-/*
- *Main -prints text
- *returns 0 if successful
- */
-int main(void)/*description*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
