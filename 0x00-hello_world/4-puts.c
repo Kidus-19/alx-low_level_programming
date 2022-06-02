35a216eaafc0a81711e7b32660664397e074893f
Kidus-19 <kidusfesseha@gmail.com>
2022-06-02 15:37:14 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Write a C program that prints exactly "Programming is like building a multilingual puzzle,
+ *  main: prints exactly "Programming is like building a multilingual puzzle,
  *  followed by a new line.
  *  Returns: 0 (Success)
  */
