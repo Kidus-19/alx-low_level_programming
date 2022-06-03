e97a1a00f7d91771e3246bab5630853babf2a357
Kidus-19 <kidusfesseha@gmail.com>
2022-06-03 14:33:57 -0700

    0-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * a program that prints all possible combinations of
+ * single-digit numbers.Numbers must be separated by ,,
+ * followed by a spaceNumbers should be printed
+ * in ascending order
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i=48;
+	while (i <= 57)
+	{
+		putchar(i);
+		if ( i== 57)
+		{
+			putchar('\n');
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
