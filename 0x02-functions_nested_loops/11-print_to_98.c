a968edacdd7a807e1db383319d4240d7a2081b8e
Kidus-19 <kidusfesseha@gmail.com>
2022-06-07 19:57:52 -0700

    11- print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,12 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n < 98)
+	if (n < '98')
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i <= '98'; i++)
 		{
 			_putchar(i);
-			if (i == 98)
+			if (i == '98')
 				_putchar('\n');
 			else
 			{
@@ -25,10 +25,10 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i >= '98'; i--)
 		{
 			_putchar(i);
-			if (i == 98)
+			if (i == '98')
 				_putchar('\n');
 			else
 			{
