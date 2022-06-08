f5edc58ac6ae224dfdef8d8e27f151e9623b1b63
Kidus-19 <kidusfesseha@gmail.com>
2022-06-07 19:43:49 -0700

    10-sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: input number
+ * @b: input number
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum =  a + b;
+	return (sum);
+}
