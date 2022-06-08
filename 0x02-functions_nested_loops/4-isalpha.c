b00b94952412b440ee3f0167d1a592b039d0b673
Kidus-19 <kidusfesseha@gmail.com>
2022-06-07 19:17:24 -0700

    3-_isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,17 @@
-#include "main.h"                                            
-                                                             
-/**                                                          
+#include "main.h"
+
+/**
  * _isalpha - checks for alphabet
  * @c: The character to check
  * Return: On success 1.
  * On error, 0 is returned, and errno is set appropriately.
- * */                                                          
-                                                             
-int _isalpha(int c);                                          
+ * */
+int _isalpha(int c);
 {
-	if (c >= 97 && c <= 123)                             
-                return (1);                                  
-        else if (c >= 65 && c <= 91)
+	if (c >= 97 && c <= 123)
+		return (1);
+	else if (c >= 65 && c <= 91)
 		return (1);
 	else
-		return (0);	
-}                                                            
+		return (0);
+}
