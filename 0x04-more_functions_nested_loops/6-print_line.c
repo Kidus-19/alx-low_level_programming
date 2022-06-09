b9cd62d70531ab2bf817f33ea728e8ae6101ccd1
Kidus-19 <kidusfesseha@gmail.com>
2022-06-09 00:24:20 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line ___
+ * @n: number of _
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
