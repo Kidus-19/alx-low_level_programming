a012a039859f85fe731c2b84b61c4e845b90aa05
Kidus-19 <kidusfesseha@gmail.com>
2022-06-08 22:14:11 -0700

    0-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0-9)
+ * @c: input integral value
+ * Return: 1 (c is digit) otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
