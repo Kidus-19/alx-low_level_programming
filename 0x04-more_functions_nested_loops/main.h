25e485d23b06ae111ae48b1a24ade55ea4016f57
Kidus-19 <kidusfesseha@gmail.com>
2022-06-10 00:18:15 -0700

    101-print_numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,3 +10,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _pow(int digits);
+int _sum(int n, int digits);
+int _nodigits(int n);
