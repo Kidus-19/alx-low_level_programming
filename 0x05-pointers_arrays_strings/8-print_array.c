fa760bf08e018a8e0b3350988655cbc945a566b3
Kidus-19 <kidusfesseha@gmail.com>
2022-06-13 09:58:05 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints an array.
+ * @a: integer pointer
+ * @n: size of the array.
+ */
+
+void print_array(int *a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
+}
