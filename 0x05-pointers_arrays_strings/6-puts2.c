8562704f2e7b611166374721966f15e36675338b
Kidus-19 <kidusfesseha@gmail.com>
2022-06-13 08:44:15 -0700

    6-putsc

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * puts2 - prints half.
+ * @str:  the string
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	len = _strlen(str) / 2;
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of the string
+ * @s: string
+ * Return: counter
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+	int i;
+
+	counter = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	return (counter);
+}
