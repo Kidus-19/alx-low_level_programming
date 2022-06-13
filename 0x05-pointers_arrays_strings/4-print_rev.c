601ca95646fe336315a5e0961bcc8d441a592a26
Kidus-19 <kidusfesseha@gmail.com>
2022-06-13 07:21:45 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in a reverse order
+ * @s: char pointer
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int len;
+
+	len = _strlen(s);
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of the string
+ * @s: string
+ * Return: counter
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+	int i;
+
+	counter = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	return (counter);
+}
