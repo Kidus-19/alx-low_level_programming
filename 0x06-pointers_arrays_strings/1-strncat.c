b07da2cd184e97fc17dbae19da4874aaca3c7272
Kidus-19 <kidusfesseha@gmail.com>
2022-06-16 07:29:15 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strncat - appends n chars of src to dest
+ * @dest: first string
+ * @src: second string to append on dest
+ * @n: n characters of src that are appended
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = _strlen(dest);
+	int i;
+
+	for (i = 0; i < n - 1 && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = src[i];
+	return (dest);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @str:  a string parameter
+ * Return: len
+ */
+
+int _strlen(char *str)
+{
+	int len;
+	int i;
+
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
