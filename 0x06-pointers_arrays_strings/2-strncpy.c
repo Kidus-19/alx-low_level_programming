18bf53321d9dcbdffac90870b10603fb3490d2bb
Kidus-19 <kidusfesseha@gmail.com>
2022-06-16 08:35:29 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int srclen;
 
-	srclen = _strlen(src);
-	for (i = 0; i < n && i < srclen; i++)
+	/*srclen = _strlen(src);*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
 
