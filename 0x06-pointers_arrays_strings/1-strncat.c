11ba4c423d50becedbe911d27a31a63b1a219e75
Kidus-19 <kidusfesseha@gmail.com>
2022-06-16 07:33:36 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,6 +13,8 @@ char *_strncat(char *dest, char *src, int n)
 	int dest_len = _strlen(dest);
 	int i;
 
+	if (n == 0)
+		return (dest);
 	for (i = 0; i < n - 1 && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
 	dest[dest_len + i] = src[i];
