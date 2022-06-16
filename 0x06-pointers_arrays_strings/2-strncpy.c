690f268a4600a93cb3875496756cfecf14f205bc
Kidus-19 <kidusfesseha@gmail.com>
2022-06-16 08:06:55 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,15 +5,37 @@
  * @dest: first string
  * @src: second string
  * @n: number of characters to get copied
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int srclen;
 
-	for (i = 0; i < n; i++)
+	srclen = _strlen(src);
+	for (i = 0; i < n && i < srclen; i++)
 	{
 		dest[i] = src[i];
 	}
 	return (dest);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @str:  a string parameter
+ * Return: len
+ */
+
+int _strlen(char *str)
+{
+	int len;
+	int i;
+
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
 }
