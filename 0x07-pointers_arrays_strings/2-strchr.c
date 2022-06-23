c7943eb05bbde00ab3e9dd434ec66af4732acf9a
Kidus-19 <kidusfesseha@gmail.com>
2022-06-23 08:00:07 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,9 +16,8 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-			return (s + i + 1);
+			return (s + i);
 	}
 	if (s[i] == '\0')
-		return (s + i);
-	return ('\0');
+		return (NULL);
 }
