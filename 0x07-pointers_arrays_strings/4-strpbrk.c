e9dd54dc948264656f8728361d4a39511ab99ec6
Kidus-19 <kidusfesseha@gmail.com>
2022-06-23 07:41:21 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,18 +5,20 @@
  * string s of any of the bytes in the string accept
  * @s: input string
  * @accept: comparision string
- * Return: str;
+ * Return: s;
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
 	int j;
+	int k;
 	int cnt;
 	char str[100];
 
 	cnt = 0;
 	j = 0;
+	k = 0;
 	while (s[j] != '\0')
 	{
 		for (i = 0; accept[i] != '\0'; i++)
@@ -24,10 +26,16 @@ char *_strpbrk(char *s, char *accept)
 			if (accept[i] == s[j])
 			{
 				str[cnt++] = s[j];
+				break;
 			}
 		}
 		j++;
 	}
 	str[cnt] = '\0';
-	return (str);
+	for (k = 0; str[k] != '\0'; k++)
+	{
+		s[k] = str[k];
+	}
+	s[k] = '\0';
+	return (s);
 }
