d5b27235585f4ea438b067743970fa2d61a706ab
Kidus-19 <kidusfesseha@gmail.com>
2022-06-23 01:01:44 -0700

    100-palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * is_palindrome - a function that returns 1
+ * if a string is a palindrome and 0 if not.
+ * @s: the string.
+ * Return: 1(success, is Palindrome) otherwise 0
+ */
+
+int is_palindrome(char *s)
+{
+	return (_isPal(s, s + _strlen(s) - 1));
+}
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: The  string.
+ * Return: len
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen(++s));
+	}
+}
+
+/**
+ * _isPal - finds the palindrome state of string.
+ * @start: a ponter to the string.
+ * @ends: a pointer to last character.
+ * Return: 1 (if pal is true), otherwise 0.
+ */
+
+int _isPal(char *start, char *ends)
+{
+	if (*start != *ends)
+	{
+		return (0);
+	}
+	if (start >= ends)
+	{
+		return (1);
+	}
+	else
+	{
+		start++;
+		ends--;
+	}
+	return (1);
+}
