96aea40ad22b94fea81618981c929e066d501d5c
Kidus-19 <kidusfesseha@gmail.com>
2022-06-30 13:02:37 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,4 +11,23 @@
 
 int *array_range(int min, int max)
 {
+	int i;
+	int j;
+	int *range;
+
+	j = 0;
+	if (min > max)
+	{
+		return ('\0');
+	}
+	range = malloc(sizeof(int) * (max - min + 1));
+	if (range)
+	{
+		for (i = min; i <= max; i++)
+		{
+			range[j++] = i;
+		}
+		return (range);
+	}
+	return ('\0');
 }
