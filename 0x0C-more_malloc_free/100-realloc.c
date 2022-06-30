b268c7fb3d557d57f56291f3f8414c95d11aa8db
Kidus-19 <kidusfesseha@gmail.com>
2022-06-30 14:20:28 -0700

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates memory in the heap.
+ * @ptr: void pointer
+ * @old_size: old size of the memory
+ * @new_size: new size of the memory
+ * Return: no return
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newPtr;
+
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+	if (new_size > old_size)
+	{
+		newPtr = realloc(ptr, new_size);
+		if (newPtr)
+		{
+			return (newPtr);
+		}
+	}
+	if (ptr == '\0')
+	{
+		newPtr = malloc(new_size);
+		if (newPtr)
+		{
+			return (newPtr);
+		}
+	}
+	if (new_size == 0 && ptr != '\0')
+	{
+		free(ptr);
+		return ('\0');
+	}
+}
