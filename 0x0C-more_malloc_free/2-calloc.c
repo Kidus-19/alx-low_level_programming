c7c1069a307ee183d702e65b7a5e63bce933b552
Kidus-19 <kidusfesseha@gmail.com>
2022-06-30 12:38:50 -0700

    2 & 3

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of members(elements)
+ * @size: size of the array.
+ * Return: no return
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	;
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - sorts ASC
+ * @min: assumed min num.
+ * @max: assumed max num.
+ * Return: integer pointer
+ */
+
+int *array_range(int min, int max)
+{
+}
