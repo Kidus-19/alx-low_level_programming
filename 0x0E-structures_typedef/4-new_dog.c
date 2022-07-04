96b2bb2f09a57372200c47609a6cfcc5c98e62f3
Kidus-19 <kidusfesseha@gmail.com>
2022-07-04 13:37:48 -0700

    4-new_dog.cwq

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog.
+ * @name: dog's name
+ * @age: dog'sage
+ * @owner: dog's owner
+ * Return: n_dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *n_dog;
+
+	n_dog = malloc(sizeof(struct dog));
+	if (n_dog)
+	{
+		n_dog->name = name;
+		n_dog->age = age;
+		n_dog->owner = owner;
+		return (n_dog);
+	}
+	return ('\0');
+}
+
