cb425164b6e05d355241d41720409b308bc9f844
Kidus-19 <kidusfesseha@gmail.com>
2022-07-04 09:42:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct of some dog properties
+ * @name: First member
+ * @age: Second member
+ * @owner: Third membeer
+ *
+ * Description: age, name, and owner of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* DOG_H */
