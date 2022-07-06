1d376090a926447e490374242d52e9a470d4ff77
Kidus-19 <kidusfesseha@gmail.com>
2022-07-06 09:54:44 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cstdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name based on different functions.
+ * @name: a pointer to character
+ * @f: a function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	*f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef FUNPTR_H
+#define FUNPTR_H
+void print_name(char *name, void (*f)(char *));
+#endif /* FUNPTR_H */
