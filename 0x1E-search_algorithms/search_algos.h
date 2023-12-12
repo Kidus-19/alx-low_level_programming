a907f38b8ce4d181e69f652bc2ddd3fa83d5c2d0
Kidus-19 <105101940+Kidus-19@users.noreply.github.com>
2023-12-12 10:56:02 +0300

    linear search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,9 @@
-#ifndef __S_A__
-#define __S_A__
+#ifndef LINEAR_SEARCH_H
+#define LINEAR_SEARCH_H
 
 #include <stdlib.h>
-#include <stdio.h> 
-
+#include <stdio.h>
+/* Function declaration(s) */
 int linear_search(int *array, size_t size, int value);
 
-#endif
+#endif /* LINEAR_SEARCH_H */
